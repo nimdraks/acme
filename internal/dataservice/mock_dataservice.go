7465153c135e5af6b0de4574f8c8e7b9f31e9665
nimdrak <nimdrak@naver.com>
2022-10-10 19:44:43 +0900

    Use MockDataService at server

---
+++ internal/dataservice/mock_dataservice.go
@@ -0,0 +1,21 @@
+package dataservice
+
+type MockDataService struct {
+}
+
+func InitMockDataService(DSN string) *MockDataService {
+	return &MockDataService{}
+}
+
+func (d *MockDataService) Load(id int) (*Person, error) {
+
+	return &Person{ID: 1, FullName: "John", Phone: "0123456780", Currency: "USD", Price: 100}, nil
+}
+
+func (d *MockDataService) LoadAll() ([]*Person, error) {
+	return nil, nil
+}
+
+func (d *MockDataService) Save(fullName, phone, currency, price string) int {
+	return -1
+}
