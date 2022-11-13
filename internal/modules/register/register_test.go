360f7c9b793ee3790868cc50f561890bca3049e4
nimdrak <nimdrak@naver.com>
2022-11-13 20:21:51 +0900

    Refactor register by using config interface

--- internal/modules/register/register_test.go
+++ internal/modules/register/register_test.go
@@ -2,6 +2,8 @@ package register
 
 import (
 	"context"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/PacktPublishing/Hands-On-Dependency-Injection-in-Go/ch04/acme/internal/modules/data"
@@ -9,18 +11,33 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-type mockConfig struct{}
+type mockOutConfig struct{}
 
-func (m *mockConfig) GetBasePrice() float64 {
+func (m *mockOutConfig) GetBasePrice() float64 {
 	return 100
 }
-func (m *mockConfig) GetExchangeRateBaseURL() string {
+func (m *mockOutConfig) GetExchangeRateBaseURL() string {
 	return "https://api.apilayer.com/currency_data"
 }
-func (m *mockConfig) GetExchangeRateAPIKey() string {
+func (m *mockOutConfig) GetExchangeRateAPIKey() string {
 	return "KqLgYfBfNgCgGfHG6UFdJWp3qOdaoGYc"
 }
 
+type mockInConfig struct {
+	testUrl string
+}
+
+func (m *mockInConfig) GetBasePrice() float64 {
+	return 100
+}
+
+func (m *mockInConfig) GetExchangeRateBaseURL() string {
+	return m.testUrl
+}
+
+func (m *mockInConfig) GetExchangeRateAPIKey() string {
+	return "test"
+}
 func TestRegisterer_Do(t *testing.T) {
 	// inputs
 	in := &data.Person{
@@ -30,10 +47,81 @@ func TestRegisterer_Do(t *testing.T) {
 	}
 
 	// call method
-	registerer := NewRegisterer(&mockConfig{})
+	registerer := NewRegisterer(&mockOutConfig{})
 	ID, err := registerer.Do(context.TODO(), in)
 
 	// validate expectations
 	require.NoError(t, err)
 	assert.True(t, ID > 0)
+}
+
+func TestRegisterer_Do_Refactored(t *testing.T) {
+	// inputs
+	in := &data.Person{
+		FullName: "Chang",
+		Phone:    "11122233345",
+		Currency: "AUD",
+	}
+
+	for _, test := range []struct {
+		name    string
+		handler http.HandlerFunc
+	}{
+		{
+			name: "Happy",
+			handler: func(w http.ResponseWriter, r *http.Request) {
+				payload := []byte(`
+				{
+				   "success":true,
+				   "historical":true,
+				   "date":"2010-11-09",
+				   "timestamp":1289347199,
+				   "source":"USD",
+				   "quotes":{
+					  "USDAUD":0.989981
+				   }
+				}`)
+				w.Write(payload)
+			},
+		},
+		{
+			name: "Not Happy1",
+			handler: func(w http.ResponseWriter, r *http.Request) {
+				payload := []byte(`
+				{
+				   "success":true,
+				   "historical":true,
+				   "date":"2010-11-09",
+				   "timestamp":1289347199,
+				   "source":"USD",
+				   "quotes":{
+					  "USDCNY":0.989981
+				   }
+				}`)
+				w.Write(payload)
+			},
+		},
+		{
+			name: "Not Happy2",
+			handler: func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(500)
+			},
+		},
+	} {
+		// set Servers
+		server := httptest.NewServer(test.handler)
+		defer server.Close()
+
+		// call method
+		registerer := NewRegisterer(&mockInConfig{server.URL})
+		ID, err := registerer.Do(context.TODO(), in)
+
+		// validate expectations
+		if test.name == "Happy" {
+			require.NoError(t, err)
+			assert.True(t, ID > 0)
+		} else {
+			require.Error(t, err)
+		}
+	}
 }
